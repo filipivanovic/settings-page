03ee9534dc17ca8047568378faef0d62f7da2707
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-15 19:55:42 +0200

    create TabLink components and types

---
+++ src/components/TabLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Tab } from '@/types'
+
+defineProps<{
+  currentTab: string,
+  tab: Tab
+}>()
+
+defineEmits<{
+    click: []
+}>()
+
+</script>
+
+<template>
+<div>Link</div>
+</template>
+
+<style scoped>
+
+</style>
