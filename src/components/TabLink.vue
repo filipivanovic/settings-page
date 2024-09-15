ea615fc1e1c128b9cd81deeec2ce7f26b35b80bf
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-15 20:36:10 +0200

    render tabs in navigation

--- src/components/TabLink.vue
+++ src/components/TabLink.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import type { Tab } from '@/types'
+import { computed } from 'vue'
 
-defineProps<{
+const isActive = computed(() => props.currentTab === props.tab.key)
+
+const props = defineProps<{
   currentTab: string,
   tab: Tab
 }>()
@@ -13,7 +16,7 @@ defineEmits<{
 </script>
 
 <template>
-<div>Link</div>
+  <button :class="{'p-4 border-b-2': true, 'border-transparent': !isActive, 'border-b-blue-600 text-blue-600': isActive}" @click="$emit('click')">{{ tab.label }}</button>
 </template>
 
 <style scoped>
