0d9b6a61d328d8a899d4444a689758c072f9790b
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-16 18:16:41 +0200

    typescript optimization

--- src/composables/useSettings.ts
+++ src/composables/useSettings.ts
@@ -1,5 +1,13 @@
 import { ref, watch } from 'vue'
 import type { Visibility } from '@/types'
+
+interface settingsMap {
+	general: GeneralSettings,
+	notifications: NotificationsSettings,
+	privacy: PrivacySettings
+}
+type SettingsKey = keyof settingsMap
+
 interface GeneralSettings {
 	username: string,
 	email: string,
@@ -18,31 +26,44 @@ interface PrivacySettings {
 	searchEngineIndexing: boolean
 }
 
-const general = ref<GeneralSettings>((() => {
-	const stored = localStorage.getItem('general')
-	return stored !== null ? JSON.parse(stored) : {
+const init = <T extends SettingsKey>(key: T, defaults: settingsMap[T]) => {
+	const stored = localStorage.getItem(key)
+	return stored !== null ? JSON.parse(stored) : defaults
+}
+
+const watcher = <T extends SettingsKey>(key: T) => (value: settingsMap[T]) => {
+	localStorage.setItem('general', JSON.stringify(value))
+}
+
+const general = ref<GeneralSettings>(
+	init('general', {
 		username: '',
 		email: '',
 		about: '',
 		gender: 'male',
 		country: 'Serbia'
-	}
-})())
+	})
+)
 
-watch(general, (value) => {
-	localStorage.setItem('general', JSON.stringify(value)), { deep: true }
-})
+watch(general, watcher('general'), { deep: true })
 
-const notifications = ref<NotificationsSettings>({
-	email: false,
-	sms: false
-})
+const notifications = ref<NotificationsSettings>(
+	init('notifications', {
+		email: false,
+		sms: false
+	})
+)
 
-const privacy = ref<PrivacySettings>({
-	visibility: 'public',
-	searchEngineIndexing: false
-})
+watch(notifications, watcher('notifications'), { deep: true })
 
+const privacy = ref<PrivacySettings>(
+	init('privacy', {
+		visibility: 'public',
+		searchEngineIndexing: false
+	})
+)
+
+watch(privacy, watcher('privacy'), { deep: true })
 
 export function useSettings() {
 	return {
--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ import type { Component } from 'vue'
 export type TabKey = 'General' | 'Notifications' | 'Privacy'
 export type Visibility = 'public' | 'private'
 
+
 export interface Tab {
   key: TabKey,
   label: string,
